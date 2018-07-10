990e2fd7e052a2550827853a1ebf858a47d988ef
farfaniriss <iris.farfan.c@gmail.com>
2018-07-10 17:27:19 -0500

    progressive image loading with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  ready = false;
+  coverImageTest = "https://data.whicdn.com/images/297083770/large.jpg";
+  thumbTest = "http://i843.photobucket.com/albums/zz354/DarknessOfMyLife/6-resized_zps8l4xcevi.jpg";
+
+ngOnInit(){
+  this.loadImage();
 }
+
+
+  loadImage(){
+    let img = new Image();
+    img.src = "https://data.whicdn.com/images/297083770/large.jpg";
+    img.onload = () =>  this.ready = true;
+  }
+
+}
+
